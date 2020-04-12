3b1763ba79c7d670e7eb710ea1d71d2cb845c965
Eric Régnier <utopman@gmail.com>
2020-04-12 14:46:56 +0200

    fix(ui): output download close #96

--- ui/src/components/executions/Overview.vue
+++ ui/src/components/executions/Overview.vue
@@ -36,7 +36,7 @@
                         class="btn btn-primary"
                         v-if="['optionalFile', 'file'].includes(row.item.key)"
                         target="_blank"
-                        :href="itemUrl(row.item.value)"
+                        :href="itemUrl(row.item.key)"
                     >{{$t('download') | cap}}</b-link>
                 </template>
             </b-table>
@@ -58,8 +58,8 @@ export default {
         Restart
     },
     methods: {
-        itemUrl(value) {
-            return `${apiRoot}executions/${this.execution.id}/file?filePath=${value.uri}&type=${value.type}`;
+        itemUrl(key) {
+            return `${apiRoot}executions/${this.execution.id}/file?key=${key}`;
         },
         restart() {
             this.$emit("follow");
